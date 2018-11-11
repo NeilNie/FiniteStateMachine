14f403e1655deee2ccc30e74ad92fa6ce3384e89
Neil Nie <yongyang.nie@gmail.com>
2018-11-11 11:44:10 -0500

    a 1 hz clock added

--- FiniteStateMachine.v
+++ FiniteStateMachine.v
@@ -1,2 +1,19 @@
 // 
 // Finite State Machine Pattern Detector
+
+module FiniteStateMachine(KEY, LEDR, CLOCK_50);
+
+input KEY;
+input CLOCK_50;
+output [17:0] LEDR;
+
+wire out_1hz;
+
+clock_1hz clock(
+	.clk(CLOCK_50), 
+	.out(out_1hz)
+);
+
+assign LEDR[17] = out_1hz;
+
+endmodule
